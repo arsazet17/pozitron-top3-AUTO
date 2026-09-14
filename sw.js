bf6e484cc011c6e3524b694ae030721cbfaac694
sarabek18 <arsazet.17.go@gmail.com>
2026-09-14 22:02:01 +0300

    TOP3 v1.3.4: refresh all-links statistics cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="top3-auto-v133-triple-all-links";
+const CACHE="top3-auto-v134-triple-all-links-stats";
 const STATIC=["./","./index.html","./styles.css","./top3-upgrade.css","./js/app.js","./js/storage.js","./js/engine/chat-master.js","./js/engine/mirror15.js","./js/engine/triples-chat.js","./js/engine/triple-all-links.js","./js/pages/home.js","./js/pages/archive.js","./js/pages/mirror.js","./js/pages/triples.js","./assets/icon.png","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).catch(()=>{}))});
 self.addEventListener("activate",e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
