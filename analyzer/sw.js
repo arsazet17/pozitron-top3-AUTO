dc2a91ebdca13fe7944426b0e028464e4084a776
sarabek18 <arsazet.17.go@gmail.com>
2026-09-21 01:49:16 +0300

    Analyzer v0.6.2 cache bump

--- analyzer/sw.js
+++ analyzer/sw.js
@@ -1,4 +1,4 @@
-const CACHE='top3-analyzer-v0.6.1';
+const CACHE='top3-analyzer-v0.6.2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./engine.js','./seed.json','./manifest.webmanifest','../assets/icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
